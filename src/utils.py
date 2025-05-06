bc14e6ca77446974de876d6bbf2fdac7d33574c9
Subhash Pavan Chakravarthy Somarouthu <sspchakravarthy@gmail.com>
2025-05-06 00:05:34 -0400

    model building and tuning

--- src/utils.py
+++ src/utils.py
@@ -31,7 +31,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
+            gs = GridSearchCV(model,para,cv=2)
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
